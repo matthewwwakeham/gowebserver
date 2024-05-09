71c2373ff846204dde97f5e45f10787d0e624484
Yubel <mwakeham95@gmail.com>
2024-05-09 16:18:04 -0500

    Added project files.

---
+++ main.go
@@ -0,0 +1,37 @@
+// Set up package
+package main
+
+// Import required packages
+import (
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+// Create two functions
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got / request\n")
+	io.WriteString(w, "This is my microservice!\n")
+}
+
+func getHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got /hello request\n")
+	http.ServeFile(w, r, "hello.html")
+}
+
+// Main func
+func main() {
+
+	http.HandleFunc("/", getRoot)
+	http.HandleFunc("/hello", getHello)
+
+	err := http.ListenAndServe(":3333", nil)
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("server closed\n")
+	} else if err != nil {
+		fmt.Printf("error starting sever: %s\n", err)
+		os.Exit(1)
+	}
+}
